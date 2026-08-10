9c1644e219253d3899804b44e8f9333217267ad2
OveralZ <wangr7636@gmail.com>
2026-08-10 17:37:27 +1200

    Refactor GameManager into Roulette.java

--- core/src/main/java/io/wasabi/urg/Roulette.java
+++ core/src/main/java/io/wasabi/urg/Roulette.java
@@ -2,23 +2,30 @@ package io.wasabi.urg;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import io.wasabi.urg.screens.GameScreen;
-import io.wasabi.urg.managers.GameManager;
+import io.wasabi.urg.state.RunState;
 
 public class Roulette extends Game {
 
-    public Viewport viewport;
-    public OrthographicCamera camera;
+    private final RunState runState = new RunState();
+
+    // Renderers
+    private ShapeRenderer shapeRenderer;
+
+    private Viewport viewport;
+    private OrthographicCamera camera;
 
     @Override
     public void create() {
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(640, 480, camera); // change this depending on actual game size at launch (?)
 
-        GameManager.getInstance().initialize(this);
+        shapeRenderer = new ShapeRenderer();
+
         this.setScreen(new GameScreen(this));
     }
 
@@ -29,6 +36,9 @@ public class Roulette extends Game {
 
     @Override
     public void render() {
+        viewport.apply();
+        shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
+
         super.render();
     }
 
@@ -38,4 +48,7 @@ public class Roulette extends Game {
             getScreen().dispose();
         }
     }
+
+    public RunState getRunState() { return runState; }
+    public ShapeRenderer getShapeRenderer() { return shapeRenderer; }
 }
