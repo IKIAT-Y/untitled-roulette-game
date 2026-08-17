102f1df6a8c980709776a9076f506dafebde7990
GoldenKnight123 <hanxiangw0599@gmail.com>
2026-08-18 01:54:29 +1200

    Added a function to randomly choose card from all rarity pools

--- core/src/main/java/io/wasabi/urg/Roulette.java
+++ core/src/main/java/io/wasabi/urg/Roulette.java
@@ -55,7 +55,7 @@ public class Roulette extends Game {
         initializeCardPool();
 
         // Card testing
-        runState.addCard(getCommonCard());
+        runState.addCard(getRandomCard());
         System.out.println("Added card: " + runState.getOwnedCards().get(0).getClass().getSimpleName());
 
         this.gameScreen = new GameScreen(this);
@@ -73,14 +73,49 @@ public class Roulette extends Game {
         commonCards.add(new io.wasabi.urg.elements.card.OddCard());
     }
 
+    public Card getRandomCard() {
+        // 5% chance for rare 25% chance for uncommon 70% chance for common
+        double roll = Math.random();
+
+        if (roll < 0.05 && !rareCards.isEmpty()) {
+            return getRareCard();
+        } else if (roll < 0.3 && !uncommonCards.isEmpty()) {
+            return getUncommonCard();
+        } else {
+            return getCommonCard();
+        }
+    }
+
     public Card getCommonCard() {
         if (commonCards.isEmpty()) {
-            return null; // or throw an exception, depending on your design
+            return null; // might need to add more error handling
         }
         int index = (int) (Math.random() * commonCards.size());
 
         Card card = commonCards.get(index);
         commonCards.remove(index);
+        return card;
+    }
+
+    public Card getUncommonCard() {
+        if (uncommonCards.isEmpty()) {
+            return null;
+        }
+        int index = (int) (Math.random() * uncommonCards.size());
+
+        Card card = uncommonCards.get(index);
+        uncommonCards.remove(index);
+        return card;
+    }
+
+    public Card getRareCard() {
+        if (rareCards.isEmpty()) {
+            return null;
+        }
+        int index = (int) (Math.random() * rareCards.size());
+
+        Card card = rareCards.get(index);
+        rareCards.remove(index);
         return card;
     }
 
