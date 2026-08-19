74978ef65438ee48e04cc7940488360afdcf08ed
Susan <sshi443@aucklanduni.ac.nz>
2026-08-19 22:56:32 +1200

    Created structure for AbstractCharm class, inherits from GameObject.

---
+++ core/src/main/java/io/wasabi/urg/elements/charm/AbstractCharm.java
@@ -0,0 +1,18 @@
+package io.wasabi.urg.elements.charm;
+
+import io.wasabi.urg.elements.GameObject;
+
+public class AbstractCharm extends GameObject{
+    
+    public void update(float delta) {
+
+    }
+
+    public void render() {
+
+    }
+
+    public void dispose() {
+        
+    }
+}
